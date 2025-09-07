6238ee6524f03f950aba840b155d18ad3094edac
Jean-Nicolas Thurre <jean-nicolas.thurre@edu.hefr.ch>
2025-09-07 11:33:20 +0200

    feat: better script comments

--- Misc/upload_itchio.ps1
+++ Misc/upload_itchio.ps1
@@ -1,26 +1,38 @@
-# == Prerequisites
+# === PREREQUISITES ===
+#
 #   1. Download and install butler <https://itchio.itch.io/butler>
 #   2. Login with `butler login`
 #   3. Place this script in _PROJECT_ROOT_/Misc/upload_itchio.ps1
 #   4. Set GODOT_CONSOLE_PATH with `setx GODOT_CONSOLE_PATH "D:\OneDrive\Projets\Programmes\Godot\Godot_console.exe"`
 #   5. Open Godot and export one time with the "Web" preset
-# == Run
-#   - cd _YOUR_PROJECT_ROOT_PATH_
+#   6. Open this script and edit project constants
+#   ==> Now you can run this script
+
+# === RUN INSTRUCTIONS ===
+#
+#   - cd _PROJECT_ROOT_
 #   - powershell .\Misc\upload_itchio.ps1
+#   ==> the .zip is in .\Build folder
+#   ==> the version is uploaded to the itch.io page
+#
 # == If error
-# In cas of permissions error, use :
-#   - cd _YOUR_PROJECT_ROOT_PATH_
+# In case of permissions error, use :
+#   - cd _PROJECT_ROOT_
 #   - Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 #   - powershell -ExecutionPolicy Bypass -File ".\Misc\upload_itchio.ps1"
 
-# Computer Environment
-$GODOT_CONSOLE_PATH = $env:GODOT_CONSOLE_PATH
-
+# === PROJECT CONSTANTS ===
+#
 # Project Constants
 $BUILD_FILENAME = "severed-finger"
 $ITCH_PROJECT = "severed-finger"
 $ITCH_CHANNEL = "html5"
 $ITCH_USER = "5kender"
+
+# === SCRIPT ===
+
+# Environment variable for Godot console path
+$GODOT_CONSOLE_PATH = $env:GODOT_CONSOLE_PATH
 
 # Get script directory (Misc folder)
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
